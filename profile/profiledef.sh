8c0486cba00825cc8b4e56069738d38dbd8ae196
Mostafa Taher Ahmed <mostafa.taher.c1@gmail.com>
2026-07-26 11:01:10 +0300

    Add profile/profiledef.sh

---
+++ profile/profiledef.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="auraos"
+iso_label="AURAOS_$(date +%Y%m)"
+iso_publisher="AuraOS <https://auraos.dev>"
+iso_application="AuraOS Live Desktop"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers.d/99_auraos"]="0:0:440"
+  ["/usr/local/bin/auraos-firstboot"]="0:0:755"
+)
